077b55f96890c539405f7b50cdacee02a23f1377
esaedro <108135791+esaedro@users.noreply.github.com>
2025-06-02 12:44:13 +0200

    Aggiunti i models per le applicazioni

--- progettoiseo/articoli/models.py
+++ progettoiseo/articoli/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from utenti.models import Utente
 
 # Create your models here.
+
+class Articolo(models.Model):
+    id = models.AutoField(primary_key=True)
+    tag = models.CharField(max_length=50, blank=True, null=True)
+    titolo = models.CharField(max_length=200)
+    testo = models.TextField(blank=False, null=False)
+    autori = models.ManyToManyField(Utente, related_name='articoli', db_table='articolo_autori')
+    data_pubblicazione = models.DateTimeField(auto_now_add=True)
+    immagine = models.ImageField(upload_to='immagini_articoli/', blank=True, null=True)
+
+    def __str__(self):
+        return self.titolo
+
+    class Meta:
+        db_table = 'articoli'
+        ordering = ['-data_pubblicazione']  # Ordina per data di pubblicazione decrescente
--- progettoiseo/eventi/models.py
+++ progettoiseo/eventi/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from utenti.models import Utente
 
 # Create your models here.
+
+class Evento(models.Model):
+    id = models.AutoField(primary_key=True)
+    titolo = models.CharField(max_length=200)
+    descrizione = models.TextField(blank=False, null=False)
+    date_evento = models.JSONField(default=list)
+    luogo = models.CharField(max_length=255, blank=False, null=False)
+    immagine = models.ImageField(upload_to='immagini_eventi/', blank=True, null=True)
+    numero_partecipanti = models.PositiveIntegerField(default=0, blank=True, null=True)
+    stato = models.CharField(max_length=50, choices=[('IN_ATTESA', 'IN_ATTESA'), ('CONCLUSO', 'CONCLUSO'), ('AL_COMPLETO', 'AL_COMPLETO'), ('ANNULLATO', 'ANNULLATO')], default='IN_ATTESA')
+    organizzatore = models.ForeignKey(Utente, on_delete=models.SET_NULL, related_name='eventi_organizzati', db_column='organizzatore_id', null=True, blank=True)
+    iscritti = models.ManyToManyField(Utente, related_name='eventi_iscritti', blank=True, db_table='evento_iscritti')
+
+    def __str__(self):
+        return str(self.titolo)
+
+    class Meta:
+        db_table = 'eventi'
+        ordering = [models.F('date_evento').desc()]  # Ordina per la prima data della lista in ordine decrescente
