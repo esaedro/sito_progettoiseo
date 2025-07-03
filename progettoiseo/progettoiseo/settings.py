090bbc09e1344bd1b0754f328d98d0ef08cb9a57
cavaanna <a.cavazzini001@studenti.unibs.it>
2025-07-03 22:42:19 +0200

    non so

--- progettoiseo/progettoiseo/settings.py
+++ progettoiseo/progettoiseo/settings.py
@@ -87,10 +87,10 @@ DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'progettoiseo',
-        'USER': 'django',
-        'PASSWORD':'django',
+        'USER': 'root',
+        'PASSWORD':'root',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '3307',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
